f15abdb935bbb61841b764dcb50073f8fa32fb97
zubayermunna <muna700064@gmail.com>
2023-03-02 00:47:59 +0600

    added copyright

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,28 @@
-import React from 'react';
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
-import { logo } from './assets';
-import { Home, CreatePost } from './page';
+import { logo } from "./assets";
+import { Home, CreatePost } from "./page";
 
 const App = () => (
   <BrowserRouter>
     <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
       <Link to="/">
-        <img src={logo} alt="logo" className="w-28 object-contain" />
+        <div className="flex  flex-col">
+          <img src={logo} alt="logo" className="w-28 object-contain" />{" "}
+          <p className="mt-2 text-[#666e75] text-[14px]">
+            {" "}
+            -Developed By Jobayer Munna Openai 3.0
+          </p>
+        </div>
       </Link>
 
-      <Link to="/create-post" className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md">Create</Link>
+      <Link
+        to="/create-post"
+        className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md"
+      >
+        Create
+      </Link>
     </header>
     <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
       <Routes>
